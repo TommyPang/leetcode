ad22232ba28e445c5fe7a9922256cff5a58bdd10
Tommy Pang <63743103+TommyPang@users.noreply.github.com>
2021-11-20 11:01:42 -0500

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,122 @@
+import java.util.*;
+import java.util.Deque;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Node fir = new Node(7);
+        fir.next = new Node(13);
+        fir.next.random = fir;
+        fir.next.next = new Node(11);
+        fir.next.next.next = new Node(10);
+        fir.next.next.next.random = fir.next.next;
+        fir.next.next.next.next = new Node(1);
+        fir.next.next.next.next.random = fir;
+        fir.next.next.random = fir.next.next.next.next;
+        ListNode sec = new ListNode(1);
+        sec.next = new ListNode(2);
+        sec.next.next = new ListNode(3);
+        sec.next.next.next = new ListNode(4);
+        sec.next.next.next.next = new ListNode(5);
+        TreeNode forth = new TreeNode(3);
+        forth.left = new TreeNode(1);
+        forth.right = new TreeNode(4);
+        forth.left.right = new TreeNode(2);
+        forth.right.right = new TreeNode(5);
+        Solution s = new Solution();
+
+    }
+    public boolean PredictTheWinner(int[] nums) {
+        int n = nums.length;
+        // dp[i][j][k] -> given the interval [i-j], the maximum point current player k(0/1) can get.
+        int[][][] dp = new int[n][n][2]; // 1 means the player currently making the choice and 0 is the opponent
+        int total = 0;
+        for (int i = 0; i<n; i++) { dp[i][i][1] = nums[i]; total+=nums[i]; }
+        for (int len = 1; len<n; len++) {
+            for (int l = 0; l+len<n; l++) {
+                int r = l+len;
+                dp[l][r][1] = Math.max(dp[l+1][r][0] + nums[l], dp[l][r-1][0] + nums[r]);
+                dp[l][r][0] = Math.min(dp[l+1][r][1], dp[l][r-1][1]);
+            }
+        }
+        return dp[0][n-1][1]>=total-dp[0][n-1][1];
+    }
+
+
+
+
+
+
+
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+    /*
+    public static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    static class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+     */
+}
