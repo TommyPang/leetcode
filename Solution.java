d4accdfc607219566e812782891ea6f39b47f30e
Tommy Pang <63743103+TommyPang@users.noreply.github.com>
2021-11-28 21:00:19 -0500

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,89 @@
+import java.util.*;
+import java.util.Deque;
+
+class Solution {
+
+    public int findIntegers(int n) {
+        return 1 + dfs(n, 1);
+    }
+    public int dfs(int n, int cur) {
+        if (cur>n) return 0;
+        if ((cur&1) == 1) return 1+dfs(n, (cur<<1));
+        else return 1 + dfs(n, (cur<<1)+1)+dfs(n, (cur<<1));
+    }
+
+
+
+
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+    /*
+    public static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    static class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+     */
+}
